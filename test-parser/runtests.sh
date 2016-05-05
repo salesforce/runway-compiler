e0a0b3a6d400c33e500452c0ac22cc6e0062fd27
Diego Ongaro <dongaro@salesforce.com>
2016-05-05 11:53:24 -0700

    Remove examples/
    These weren't the best examples, might as well not bundle them in.

--- test-parser/runtests.sh
+++ test-parser/runtests.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-echo "parser test: input.model"
-test-parser/runtest.sh test-parser/input.model test-parser/output.json
-
-echo "parser test: tokenring.model"
-test-parser/runtest.sh examples/tokenring/tokenring.model test-parser/output-tokenring.json
+for t in 1 2; do
+  echo "parser test: input-${t}.model"
+  test-parser/runtest.sh test-parser/input-${t}.model test-parser/output-${t}.json
+done
